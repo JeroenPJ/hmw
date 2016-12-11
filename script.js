e3e51f14c04d328777ad988341759f8cdd267258
Jeroen Jagt <jeroenpjagt@gmail.com>
2016-12-11 19:13:51 +0100

    added bubbles

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 $(document).ready(function(){
+  function createBubble() {
+    var size = Math.floor(Math.random() * 25) + 7;
+    var duration = Math.floor(Math.random() * 75) + 300;
+    var x = Math.floor(Math.random() * 100);
+    var y = Math.floor(Math.random() * 100);
+    var $new_bubble = "<div class='bubble' style='left:" + x + "%; top:" + y + "%; border-width: 1px'></div>";
+    $($new_bubble).appendTo('#bubble-field').animate({ "top": "-=" + size / 2,
+                                                       "left": "-=" + size / 2,
+                                                       "width": size + "px",
+                                                       "height": size + "px"
+                                                     }, duration, "easeInOutQuad", function(){
+      // $(this).animate({ "width": size, "height": size }, duration * 2, "easeInOutQuad", function() {
+        $(this).css('border-width', '0');
+      // });
+    });
+  }
+
+  function createBubbleField() {
+    $('#bubble-field').animate({ "top": "30px" }, 1200, "linear");
+    for(var i = 0; i < 35; i += 1) {
+      setTimeout(createBubble, i * 30);
+    }
+  }
+
   function one() {
-    $('body').append("<div id='one' class='bullet black align-center op-top'></div><div id='two' class='bullet black align-center op-bottom'></div>");
+    $('body').append("<div id='one' class='bullet black align-center op-top'></div>" +
+                     "<div id='two' class='bullet black align-center op-bottom'></div>")
+             .append("<div id='bubble-field'></div>");
     $('.bullet').css({ left: ($(window).width() - 15) / 2 })
                 .animate({ top: ($(window).height() - 15) / 2 },
                          1000, "easeInOutQuad");
@@ -33,11 +59,7 @@ $(document).ready(function(){
       "height": "1px"
     }, 800, "easeInOutQuad");
 
-    setTimeout(four, 800);
-  }
-
-  function four() {
-    $('body').append("<h1>" +
+    $('body').append("<h1 id='name'>" +
                       "<span id='h'>H</span>" +
                       "<span id='a1'>a</span>" +
                       "<span id='r1'>r</span>" +
@@ -49,14 +71,15 @@ $(document).ready(function(){
                       "<span id='a2'>a</span>" +
                       "<span id='m'>m</span>" +
                       "<span id='s'>s</span>" +
-                      "</h1>")
-             .animate({
-      "background-color": "#1a1a1a"
-    }, 1400, "easeInOutQuad");
+                      "</h1>");
 
-    $('h1').css({
-      "left": ($(window).width() - $('h1').width()) / 2,
-      "top": ($(window).height() - $('h1').height()) / 2
+    setTimeout(four, 800);
+  }
+
+  function four() {
+    $('#name').css({
+      "left": ($(window).width() - $('#name').width()) / 2,
+      "top": ($(window).height() - $('#name').height()) / 2
     });
 
     var text_color = "#fff";
@@ -74,6 +97,8 @@ $(document).ready(function(){
     setTimeout(function(){ $('#a2').animate({ "color": text_color }, duration, easing); }, duration*4.5);
     setTimeout(function(){ $('#m').animate({ "color": text_color }, duration, easing); }, duration*5);
     setTimeout(function(){ $('#s').animate({ "color": text_color }, duration, easing); }, duration*5.5);
+
+    createBubbleField();
   }
 
   one();
